1860bcb12eb70e415c39c875c5c68d639fdee426
Thomas McEvoy <tmcevoy213@gmail.com>
2023-09-15 00:39:37 -0400

    got it workin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,111 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount, onDestroy } from 'svelte';
+	import Exercise from './exercise.svelte';
+
+	export let exercises = [
+		{ name: 'squat', trainingMax: 130, isVisible: true },
+		{ name: 'bench', trainingMax: 140, isVisible: true },
+		{ name: 'deadlift', trainingMax: 235, isVisible: true },
+		{ name: 'press', trainingMax: 100, isVisible: true }
+	];
+
+	export const weeks = ['I', 'II', 'III', 'IV'];
+
+	onMount(async () => {
+		// squat = await localStorage.getItem('squat') || squat;
+		// bench = await localStorage.getItem('bench') || bench;
+		// deadlift = await localStorage.getItem('deadlift') || deadlift;
+	});
+
+	onDestroy(() => {
+		// localStorage.setItem('squat', squat);
+		// localStorage.setItem('bench', bench);
+		// localStorage.setItem('deadlift', deadlift);
+	});
+</script>
+
+<div id="page">
+	{#each weeks as week, i}
+		<div class="week">
+			<h2>Week {week}</h2>
+			{#each exercises as exercise}
+				{#if exercise.isVisible === true}
+					<Exercise bind:exercise weekNumber={i} />
+				{/if}
+			{/each}
+		</div>
+	{/each}
+</div>
+
+<style>
+	:global(*) {
+		box-sizing: border-box;
+	}
+
+	:global(:root) {
+		--font-size: 4vw;
+		--font-size-wide: calc(0.5rem + 0.5vw);
+		--font-color: #222;
+		--small-padding: 0.5vh;
+	}
+
+	:global(body) {
+		margin: 0;
+	}
+
+	:global(h2),
+	:global(h3),
+	:global(h4),
+	:global(input) {
+		height: 1.25rem;
+		margin: 0;
+		padding: var(--small-padding) 0;
+		font: inherit;
+		font-weight: bold;
+	}
+
+	#page {
+		width: 100%;
+		margin: 0;
+		border: 6px var(--font-color) double;
+		padding-bottom: 2vw;
+		padding-left: 3vw;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		font: var(--font-size) / 1 helvetica;
+		overflow: hidden;
+	}
+	@media (min-width: 414px) {
+		#page {
+			padding-left: 0.75em;
+			font-size: var(--font-size-wide);
+		}
+	}
+	@media (min-width: 850px) {
+		#page {
+			height: 100vh;
+			padding-left: 0.33em;
+		}
+	}
+
+	.week {
+		padding-top: 0.667em;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+	@media (min-width: 414px) {
+		.week {
+			margin-top: 0;
+			width: 50%;
+		}
+	}
+	@media (min-width: 850px) {
+		.week {
+			padding-top: 0.4em;
+			width: 25%;
+		}
+	}
+</style>
---
+++ src/routes/workout.svelte
@@ -0,0 +1,61 @@
+<script>
+	export let weekNumber,
+		trainingMax,
+		increment = 5;
+
+	console.log(weekNumber);
+
+	export let sets = [
+		[0.65, 0.7, 0.75, 0.4],
+		[0.75, 0.8, 0.85, 0.5],
+		[0.85, 0.9, 0.95, 0.6]
+	];
+
+	export function roundDown(weight) {
+		if (!isNaN(weight)) return Math.floor(weight / increment) * increment;
+		return 0;
+	}
+</script>
+
+<div class="workout">
+	<div class="reps column">
+		<div class="column-title">Reps</div>
+		<div class="reps">{weekNumber === 1 ? 3 : 5}</div>
+		<div class="reps">{weekNumber === 0 || weekNumber === 3 ? 5 : 3}</div>
+		<div class="reps">
+			{weekNumber === 0 || weekNumber === 3 ? 5 : weekNumber === 1 ? 3 : 1}
+		</div>
+	</div>
+
+	<div class="weight column">
+		<div class="column-title">Weight</div>
+		{#each sets as set, i}
+			<div class="weight">{roundDown(trainingMax * set[weekNumber])}</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.workout {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+
+	.column {
+		text-align: right;
+	}
+
+	.column-title {
+		padding: var(--small-padding) 0;
+		font-style: italic;
+	}
+	.column:first-child .column-title {
+		text-align: left;
+	}
+
+	.reps,
+	.weight,
+	.column {
+		padding: var(--small-padding) 0;
+	}
+</style>
