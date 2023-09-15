73cbfaec2e876206543b86eec06c0834fda9b80b
Thomas McEvoy <tmcevoy213@gmail.com>
2023-09-15 02:45:07 -0400

    switch to plain svelte

---
+++ src/App.svelte
@@ -0,0 +1,85 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import Exercise from "./lib/Exercise.svelte";
+
+  export let exercises = [
+    { name: "squat", trainingMax: 130, isVisible: true },
+    { name: "bench", trainingMax: 140, isVisible: true },
+    { name: "deadlift", trainingMax: 235, isVisible: true },
+    { name: "press", trainingMax: 100, isVisible: true },
+  ];
+
+  export const weeks = ["I", "II", "III", "IV"];
+
+  onMount(async () => {
+    // squat = await localStorage.getItem('squat') || squat;
+    // bench = await localStorage.getItem('bench') || bench;
+    // deadlift = await localStorage.getItem('deadlift') || deadlift;
+  });
+
+  onDestroy(() => {
+    // localStorage.setItem('squat', squat);
+    // localStorage.setItem('bench', bench);
+    // localStorage.setItem('deadlift', deadlift);
+  });
+</script>
+
+<div id="page">
+  {#each weeks as week, i}
+    <div class="week">
+      <h2>Week {week}</h2>
+      {#each exercises as exercise}
+        {#if exercise.isVisible === true}
+          <Exercise bind:exercise weekNumber={i} />
+        {/if}
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<style>
+  #page {
+    width: 100%;
+    margin: 0;
+    border: 6px var(--font-color) double;
+    padding-bottom: 2vw;
+    padding-left: 3vw;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    font: var(--font-size) / 1 helvetica;
+    overflow: hidden;
+  }
+  @media (min-width: 414px) {
+    #page {
+      padding-left: 0.75em;
+      font-size: var(--font-size-wide);
+    }
+  }
+  @media (min-width: 850px) {
+    #page {
+      height: 100vh;
+      padding-left: 0.33em;
+    }
+  }
+
+  .week {
+    padding-top: 0.667em;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  @media (min-width: 414px) {
+    .week {
+      margin-top: 0;
+      width: 50%;
+    }
+  }
+  @media (min-width: 850px) {
+    .week {
+      padding-top: 0.4em;
+      width: 25%;
+    }
+  }
+</style>
---
+++ src/lib/Exercise.svelte
@@ -0,0 +1,46 @@
+<script>
+  import Workout from "./Workout.svelte";
+
+  export let weekNumber, exercise;
+</script>
+
+<div class="exercise">
+  <div class="header">
+    <h3>{exercise.name}</h3>
+    {#if weekNumber === 0}
+      <h4 class="training-max-container">
+        <input
+          type="text"
+          class="training-max"
+          bind:value={exercise.trainingMax}
+        />
+      </h4>
+    {:else}
+      <div class="training-max-placeholder" />
+    {/if}
+  </div>
+
+  <Workout {weekNumber} trainingMax={exercise.trainingMax} />
+</div>
+
+<style>
+  .header {
+    width: 33.33%;
+    text-transform: capitalize;
+  }
+
+  .training-max-container {
+    text-align: right;
+  }
+  .training-max,
+  .training-max-placeholder {
+    border: none;
+    height: 1.33rem;
+    width: 2rem;
+    text-align: center;
+  }
+  .training-max {
+    background: #eee;
+    touch-action: manipulation;
+  }
+</style>
