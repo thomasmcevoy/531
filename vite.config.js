1474093ec00e38b1dc9ab2ee2987b8a93c9fd516
Thomas McEvoy <tmcevoy213@gmail.com>
2023-09-25 22:33:55 -0400

    change base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/531-svelte/",
+  base: "/531/",
   plugins: [svelte()],
 })
